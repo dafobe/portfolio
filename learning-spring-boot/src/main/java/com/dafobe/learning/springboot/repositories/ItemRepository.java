710c6e68f785e61274ed02b15e838a84e177ae5f
dfonollosa <dfonollosa@innofis.com>
2015-07-24 13:02:50 +0200

    removed unrelated projects from repository

--- learning-spring-boot/src/main/java/com/dafobe/learning/springboot/repositories/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dafobe.learning.springboot.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dafobe.learning.springboot.model.entities.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-
-}
